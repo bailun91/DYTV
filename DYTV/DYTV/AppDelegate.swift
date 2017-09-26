217a97eb4e5563beb0f7d1c19bc000507ff69cd3
bailun91 <921880815@qq.com>
2017-09-26 11:48:10 +0800

    初始化项目

---
+++ DYTV/DYTV/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DYTV
+//
+//  Created by bailun91 on 2017/9/25.
+//  Copyright © 2017年 adel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Thread.sleep(forTimeInterval: 2)
+
+        return true
+    }
+
+
+}
+
---
+++ DYTV/DYTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DYTV
+//
+//  Created by bailun91 on 2017/9/25.
+//  Copyright © 2017年 adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
